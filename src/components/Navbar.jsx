f53faa14379909f5c0d707ae04224144ee88ab8f
marwancoder <marwane.ouarradi@gmail.com>
2025-01-27 18:29:06 +0100

    Added Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,24 +1,41 @@
-import { NavbarMenu } from "../constants"
+import { NavbarMenu } from "../constants";
+import { MdMenu } from "react-icons/md";
+import { SlEarphones } from "react-icons/sl";
+
 
 const Navbar = () => {
   return (
-    <nav>
-        {/* ____ Logo section ____ */}
-        <a href="#">
-            Playing / <span>Market</span>
-        </a>
-        {/* ____ Menu section ____ */}
-        <div>
-            <ul>
-                {NavbarMenu.map((item) => (
-                    <li key={item.id}>{item.name}</li>
-                ))}
-            </ul>
+    <>
+        <div className="bg-brandDark text-white font-varela ">
+            <nav className=" container flex justify-between items-center">
+                {/* ____ Logo section ____ */}
+                <div>
+                    <a href="#" className="text-2xl font-bold uppercase">
+                        Playing / <span className="font-extralight text-white/70">Market</span>
+                    </a>
+                </div>
+                {/* ____ Menu section ____ */}
+                <div className="hidden md:block">
+                    <ul className="flex items-center gap-5 py-4">
+                        {NavbarMenu.map((item) => (
+                            <li key={item.id}>
+                                <a href={item.link} className="inline-block text-sm py-2 px-3 uppercase">{item.name}</a>
+                            </li>
+                        ))}
+                        <button className="text-xl ps-14">
+                            <SlEarphones />
+                        </button>
+                    </ul>
+                </div>
+                {/* ____ Mobile Hamburger section ____ */}
+                <div className="md:hidden">
+                    <MdMenu className="text-4xl"/> 
+                </div>
+            </nav>
         </div>
-        {/* ____ Mobile Hamburger section ____ */}
-
-    </nav>
+    </>
   )
 }
+// install : react-icons , framer-motion , react-mouse-follower
 
 export default Navbar
